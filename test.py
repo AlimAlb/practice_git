708b387bcfd7d91029791d445eef571620083f28
Alim Albogachiev <60065438+AlimAlb@users.noreply.github.com>
2022-09-09 17:35:00 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+def func_1(a,b,c):
+    k = a + b + c
+    print(k)
+
+
+def func_2(a,b,c):
+    k = a + b + c + 'qwqwqweqweq'
+    print(b)
+
+
+def func_3(a,b,c):
+    print(c)
+
+
+def func_4(a,b,c):
+#iss 53 updates
+    print()
